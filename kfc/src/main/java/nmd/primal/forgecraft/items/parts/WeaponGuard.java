51d3bb7df3776ad6b2311ab3b26afaa30227f701
Mohammad-Ali Minaie <mminaie@bstonetech.com>
2018-10-20 18:17:53 -0400

    trying to push new files

---
+++ kfc/src/main/java/nmd/primal/forgecraft/items/parts/WeaponGuard.java
@@ -0,0 +1,10 @@
+package nmd.primal.forgecraft.items.parts;
+
+import nmd.primal.forgecraft.items.BaseItem;
+
+public class WeaponGuard extends BaseItem {
+
+    public WeaponGuard(String registryName) {
+        super(registryName);
+    }
+}
